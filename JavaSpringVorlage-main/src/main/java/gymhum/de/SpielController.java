71900df8ec73ce819093cdbc3db3c994925ee2f9
Daniel <daniel.chowanek@gymhum.de>
2023-02-07 10:17:19 +0100

    .

--- JavaSpringVorlage-main/src/main/java/gymhum/de/SpielController.java
+++ JavaSpringVorlage-main/src/main/java/gymhum/de/SpielController.java
@@ -34,6 +34,17 @@ public class SpielController {
                 getFelder()[i][k] = new Feld();
             }
         }
+
+        // Demodaten 1. Reihe
+        getFelder()[0][0].setZustand(true);
+        getFelder()[1][0].setZustand(true);
+        getFelder()[2][0].setZustand(true);
+        getFelder()[3][0].setZustand(true);
+
+        getFelder()[0][0].setIstFrei(false);
+        getFelder()[1][0].setIstFrei(false);
+        getFelder()[2][0].setIstFrei(false);
+        getFelder()[3][0].setIstFrei(false);
     }
 
     private void showTestfeld(){
@@ -44,6 +55,59 @@ public class SpielController {
             }
         }     
     }
+
+    public boolean pruefe(boolean aufX){
+        boolean pruefXo0 = aufX;
+        boolean horizontal = false;
+        boolean senkrecht = false;
+        boolean diagonal = false;
+        boolean windetected = false;
+
+        for(int i = 0; i < 6; i++){
+            for(int k = 0; k < 4; k++){
+                if(!getFelder()[i][k].getIstFrei() && !getFelder()[i][k+1].getIstFrei() && !getFelder()[i][k+2].getIstFrei() && !getFelder()[i][k+3].getIstFrei()){
+                    if(getFelder()[i][k].getZustand() == pruefXo0 && getFelder()[i][k+1].getZustand() == pruefXo0 && getFelder()[i][k+2].getZustand() == pruefXo0 && getFelder()[i][k+3].getZustand() == pruefXo0){
+                        horizontal = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 3; i++){
+            for(int k = 0; k < 7; k++){
+                if(!getFelder()[i][k].getIstFrei() && !getFelder()[i+1][k].getIstFrei() && !getFelder()[i+2][k].getIstFrei() && !getFelder()[i+3][k].getIstFrei()){
+                    if(getFelder()[i][k].getZustand() == pruefXo0 && getFelder()[i+1][k].getZustand() == pruefXo0 && getFelder()[i+2][k].getZustand() == pruefXo0 && getFelder()[i+3][k].getZustand() == pruefXo0){
+                        senkrecht = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 3; i++){
+            for(int k = 6; k > 4; k--){
+                if(!getFelder()[i][k].getIstFrei() && !getFelder()[i+1][k-1].getIstFrei() && !getFelder()[i+2][k-2].getIstFrei() && !getFelder()[i+3][k-3].getIstFrei()){
+                    if(getFelder()[i][k].getZustand() == pruefXo0 && getFelder()[i+1][k-1].getZustand() == pruefXo0 && getFelder()[i+2][k-2].getZustand() == pruefXo0 && getFelder()[i+3][k-3].getZustand() == pruefXo0){
+                        diagonal = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 3; i++){
+            for(int k = 0; k < 2; k++){
+                if(!getFelder()[i][k].getIstFrei() && !getFelder()[i+1][k+1].getIstFrei() && !getFelder()[i+2][k+2].getIstFrei() && !getFelder()[i+3][k+3].getIstFrei()){
+                    if(getFelder()[i][k].getZustand() == pruefXo0 && getFelder()[i+1][k+1].getZustand() == pruefXo0 && getFelder()[i+2][k+2].getZustand() == pruefXo0 && getFelder()[i+3][k+3].getZustand() == pruefXo0){
+                        diagonal = true;
+                    }
+                }
+            }
+        }
+        if(senkrecht || horizontal || diagonal){
+            windetected = true;
+        }
+        return windetected;
+    }
+
 
     public void setFelder(Feld[][] felder) {
         this.felder = felder;
